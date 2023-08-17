84d4427800a47714ae8454c264d8ba2a1ffb6c48
Arjun34-bit <arjundevendra34@gmail.com>
2023-08-17 14:59:40 +0530

    b

--- frontend/src/componenets/miscelleneous/SideDrawer.js
+++ frontend/src/componenets/miscelleneous/SideDrawer.js
@@ -166,7 +166,7 @@ const SideDrawer = ({}) => {
             display={{ base: "none", md: "flex" }}
           >
             Cell-Chat
-            <sub className="text beta-text">(ßeta)</sub>
+            <sub className="text beta-text">(ß)</sub>
           </Text>
         </Box>
 
