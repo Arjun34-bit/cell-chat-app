e72e8614e7bcb38faa8c7d81f9146de8ade91a9c
Arjun34-bit <arjundevendra34@gmail.com>
2023-08-19 14:17:44 +0530

    button

--- frontend/src/componenets/miscelleneous/DeleteButton.js
+++ frontend/src/componenets/miscelleneous/DeleteButton.js
@@ -70,14 +70,14 @@ const DeleteButton = ({ senderName, id, fetchChats }) => {
 
   return (
     <Menu color="black">
-      {/* <MenuButton
+      <MenuButton
         as={IconButton}
         aria-label="Options"
         icon={<DragHandleIcon />}
         _hover={{ bg: "#E8E8E8" }}
         _expanded={{ bg: "#38B2AC" }}
         variant="outline"
-      /> */}
+      />
       <MenuList color={"black"} fontSize={"20px"}>
         <MenuItem onClick={onOpen}>Profile</MenuItem>
         <MenuDivider />
