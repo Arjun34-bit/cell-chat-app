6106b00e7bcb69a9f60c74f65de884796e663513
Arjun34-bit <arjundevendra34@gmail.com>
2023-08-19 16:22:12 +0530

    few

--- frontend/src/componenets/miscelleneous/ListsBox.js
+++ frontend/src/componenets/miscelleneous/ListsBox.js
@@ -1,12 +1,59 @@
 import { Box, Text } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
+  Button,
+  useToast,
+  MenuDivider,
+} from "@chakra-ui/react";
 import React from "react";
 
-const ListsBox = () => {
+const ListsBox = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
   return (
     <Box p="4">
-      <Text>Menu Item 1</Text>
-      <Text>Menu Item 2</Text>
-      <Text>Menu Item 3</Text>
+      <Text>Profile</Text>
+      <Text onClick={isOpen}>Delete Chat</Text>
+      <Text ref={cancelRef} onClick={onClose}>
+        Close
+      </Text>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Remove {senderName}'s Chat
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={() => removeChat(id)} ml={3}>
+                Remove
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </Box>
   );
 };
