6107d8ce3d636e2591b14e7175e329e02c549b9d
Arjun34-bit <arjundevendra34@gmail.com>
2023-08-26 15:06:05 +0530

    useState

--- frontend/src/componenets/ScrollableChat.js
+++ frontend/src/componenets/ScrollableChat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ScrollableFeed from "react-scrollable-feed";
 import { ChatState } from "../Context/ChatProvider";
 import {
