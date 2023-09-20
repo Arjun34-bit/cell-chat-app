9e2da1ecc3f962ffbcc627224c39d768bee14669
Arjun34-bit <arjundevendra34@gmail.com>
2023-09-20 20:53:47 +0530

    PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,6 +18,11 @@ root.render(
     </BrowserRouter>
   </ChakraProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // ReactDOM.render(
 //   <ChakraProvider>
